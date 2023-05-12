80b503c12bdeed82730e7bae887d12a65496a59c
Raphael Saldanha <rfsaldanha@gmail.com>
2023-05-12 16:23:14 +0200

    Simplificate.

--- R/feat_max_min.R
+++ R/feat_max_min.R
@@ -2,7 +2,7 @@ feat_max <- function(x, y){
   checkmate::assert_class(x = x, classes = "tbl_ts")
   checkmate::assert_choice(x = y, choices = names(x))
 
-  x %>% tsibble::group_by_key() %>%
+  tsibble::group_by_key(.data = x) %>%
     dplyr::filter(get(y) == max(get(y)))
 }
 
@@ -10,6 +10,6 @@ feat_min <- function(x, y){
   checkmate::assert_class(x = x, classes = "tbl_ts")
   checkmate::assert_choice(x = y, choices = names(x))
 
-  x %>% tsibble::group_by_key() %>%
+  tsibble::group_by_key(.data = x) %>%
     dplyr::filter(get(y) == min(get(y)))
 }
