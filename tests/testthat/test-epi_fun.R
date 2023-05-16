121393b722d9310e245cd7f5efd4c454dc5e4b02
Raphael Saldanha <rfsaldanha@gmail.com>
2023-05-16 11:47:32 +0200

    Tests.

---
+++ tests/testthat/test-epi_fun.R
@@ -0,0 +1,24 @@
+example_data <- outbreaks::covid19_england_nhscalls_2020 %>%
+  dplyr::group_by(date, postcode) %>%
+  dplyr::summarise(count = sum(count, na.rm = TRUE)) %>%
+  dplyr::ungroup() %>%
+  tsibble::as_tsibble(key = postcode, index = date)
+
+test_that("epi_xp_tp works", {
+  res <- epi_xp_tp(example_data, "count")
+
+  expect_equal(rlang::hash(res$Xp), "59eca778e4f83fb1ff33ba052e5e9ba2")
+  expect_equal(rlang::hash(res$Tp), "fa73650ecbbba1350792a8178763b92a")
+})
+
+test_that("epi_dc3 works", {
+  res <- epi_dc3(example_data, "count")
+
+  expect_equal(rlang::hash(res$Dc3), "ceb8324a497d774f5646299237776536")
+})
+
+test_that("epi_dc6 works", {
+  res <- epi_dc6(example_data, "count")
+
+  expect_equal(rlang::hash(res$Dc6), "c6b7ab068b61bd1e60ebd964947d98a8")
+})
