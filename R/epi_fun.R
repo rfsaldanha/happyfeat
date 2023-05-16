0684c64efc77f627c51ef1c101b66b4622359c1b
Raphael Saldanha <rfsaldanha@gmail.com>
2023-05-16 11:47:26 +0200

    Epi-features funcions.

---
+++ R/epi_fun.R
@@ -0,0 +1,19 @@
+epi_xp_tp <- function(.data, y){
+  feat_max(.data, y) %>%
+    tibble::as_tibble() %>%
+    dplyr::select(
+      tsibble::key_vars(.data),
+      "Xp" = dplyr::all_of(y),
+      "Tp" = tsibble::index_var(.data)
+    )
+}
+
+epi_dc3 <- function(.data, y){
+  feat_length(.data, y, a_op = "gte", a = 3, b_op = "gte", b = 5) %>%
+    dplyr::rename("Dc3" = "freq")
+}
+
+epi_dc6 <- function(.data, y){
+  feat_length(.data, y, a_op = "gte", a = 6, b_op = "gte", b = 5) %>%
+    dplyr::rename("Dc6" = "freq")
+}
